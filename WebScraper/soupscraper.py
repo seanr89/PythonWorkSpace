7fc27ef090854d9b6f1b59c1215bbdbc639bdf03
Sean <srafferty89@gmail.com>
2020-12-16 18:30:05 +0000

    first bits of work on teh go :)

--- WebScraper/soupscraper.py
+++ WebScraper/soupscraper.py
@@ -6,3 +6,15 @@ soup = BeautifulSoup(page.content, 'html.parser')
 title = soup.title.text # gets you the text of the <title>(...)</title>
 
 print(title)
+
+centres_list = soup.find_all('article', class_ = 'venue-result-panel')
+
+#print(type(centres_list))
+#print(len(centres_list))
+
+first_record = centres_list[0]
+#print(first_record)
+print(first_record.h1.a.text)
+
+addressdiv = first_record.find('div', class_ = 'venue-result-panel__address')
+print(addressdiv.text)
